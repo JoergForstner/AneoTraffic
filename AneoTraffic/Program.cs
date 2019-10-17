e9a233b48bb923861c23a12f6cd63779ad3ccac8
Jörg Forstner <joergforstner@hotmail.com>
2019-10-17 21:28:17 +0200

    First version

--- AneoTraffic/Program.cs
+++ AneoTraffic/Program.cs
@@ -1,15 +1,43 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
+using System.IO;
 using System.Text;
-using System.Threading.Tasks;
+using System.Collections;
+using System.Collections.Generic;
 
-namespace AneoTraffic
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+class Solution
 {
-  class Program
+  static void Main(string[] args)
   {
-    static void Main(string[] args)
+    int speed = int.Parse(Console.ReadLine());
+    int lightCount = int.Parse(Console.ReadLine());
+    List<int> PossibleSpeeds = new List<int>(50);
+    for (int k = 1; k <= 200; k++) { PossibleSpeeds.Add(k); }
+
+
+    for (int i = 0; i < lightCount; i++)
     {
+      string[] inputs = Console.ReadLine().Split(' ');
+      int distance = int.Parse(inputs[0]);
+      int duration = int.Parse(inputs[1]);
+      List<int> Speeds = new List<int>();
+
+      for (int j = speed; j > 0; j--) {
+        if(Math.Floor((distance* 3.6)/(j*duration)) % 2 == 0)
+        {
+          Speeds.Add(j);
+        }
+      }
+      PossibleSpeeds = PossibleSpeeds.Intersect(Speeds).ToList();
     }
+
+    // Write an action using Console.WriteLine()
+    // To debug: Console.Error.WriteLine("Debug messages...");
+    
+    Console.WriteLine(Convert.ToString(PossibleSpeeds.Max()));
   }
 }
